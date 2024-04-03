00213f72c48055bd8744bfecc958ec875d0016a7
pudinmah <pudinmah69@gmail.com>
2024-04-03 08:01:02 +0700

    Ionic Gift Shop App - Episode 1 | Angular | Capacitor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { star } from 'ionicons/icons'
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,9 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      star
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonCol, IonRow, IonButton, IonThumbnail, IonImg, IonCard, IonItem, IonText, IonLabel, IonSearchbar } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonText, IonItem, IonCard, IonImg, IonButton, IonHeader, IonToolbar, IonTitle, IonContent,
+    IonIcon,
+    IonCol,
+    IonRow,
+    IonThumbnail,
+    IonLabel,
+    IonSearchbar
+
+  ],
 })
 export class HomePage {
-  constructor() {}
+  private api = inject(ApiService);
+  allItems :any[] = [];
+  items :any[] = [];
+  query! :string;
+
+  ngOnInit() {
+    this.getItems();
+    console.log('ngonInit homepage');
+  }
+
+  getItems() {
+    this.allItems = this.api.items;
+    this.items = [...this.allItems];
+  }
+
+  constructor() {
+
+  }
+
+  onSearchChange(event :any) {
+    console.log(event.detail.value);
+    this.query  = event.detail.value.toLowerCase();
+    this.querySearch();
+  }
+
+  querySearch(){
+    this.items = [];
+    if (this.query.length >0) {
+      this.searchItems();
+    } else {
+      this.items = [...this.allItems]
+    }
+  }
+
+  searchItems(){
+    this.items = this.api.items.filter((item)=>
+    item.name.toLowerCase().includes(this.query)
+    );
+  }
 }
